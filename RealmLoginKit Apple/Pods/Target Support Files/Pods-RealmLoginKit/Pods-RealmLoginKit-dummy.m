53aadd98eb82f39e4f0db08a6122551edf4d541e
Andrew Kharchyshyn <haawaplus@gmail.com>
2018-05-08 12:07:58 +0300

    Include pods. Build Kit with Swift 4.1

---
+++ RealmLoginKit Apple/Pods/Target Support Files/Pods-RealmLoginKit/Pods-RealmLoginKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RealmLoginKit : NSObject
+@end
+@implementation PodsDummy_Pods_RealmLoginKit
+@end
---
+++ RealmLoginKit Apple/Pods/Target Support Files/Pods-RealmLoginKitExampleTests/Pods-RealmLoginKitExampleTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RealmLoginKitExampleTests : NSObject
+@end
+@implementation PodsDummy_Pods_RealmLoginKitExampleTests
+@end
